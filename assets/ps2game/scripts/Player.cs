6a25c9e7ef54bd703f881b964b78b0766e50e861
Hasibix Hasib <hasibix3690@gmail.com>
2023-03-04 18:03:18 +0600

    Fixed player controller and readme
    Signed-off-by: Hasibix Hasib <hasibix3690@gmail.com>

--- assets/ps2game/scripts/Player.cs
+++ assets/ps2game/scripts/Player.cs
@@ -11,8 +11,7 @@ public partial class Player : CharacterBody2D {
     [Export] private float crouchSpeed = 50f;
     [Export] private float gravity = 4.5f;
     [Export] private CollisionShape2D collider;
-    [Export] private RayCast2D wallCheckRight;
-    [Export] private RayCast2D wallCheckLeft;
+    [Export] private RayCast2D wallCheck;
     [Export] private RayCast2D ceilingCheck;
     [Export] private bool isDead = false;
     [Export] private float currentHealth = 100f;
@@ -20,6 +19,7 @@ public partial class Player : CharacterBody2D {
     private Vector2 velocity;
     private Vector2 colliderSize;
     private bool canWallJump = false;
+    private bool wasFlipped = false;
 
     public override void _Ready() {
         RectangleShape2D shape = (RectangleShape2D) collider.Shape;
@@ -27,16 +27,63 @@ public partial class Player : CharacterBody2D {
     }
 
     public override void _Process(double delta) {
-        velocity = Velocity;
         isDead = currentHealth < 0;
+        if(!wasFlipped) {
+            this.Scale = new Vector2(this.Scale.X == -1 ? 1 : -1, this.Scale.Y);
+            wasFlipped = true;
+        }
     }
     
     public override void _PhysicsProcess(double delta) {
+        Control(
+            isDead,
+            Input.IsActionPressed("left"),
+            Input.IsActionPressed("right"),
+            Input.IsActionPressed("sprint"),
+            Input.IsActionPressed("crouch"),
+            airControl,
+            Input.IsActionPressed("jump"),
+            delta,
+            wallCheck.IsColliding(),
+            ceilingCheck.IsColliding(),
+            this.IsOnFloor()
+        );
+    }
+
+    void Control(bool isDead, bool moveConditionLeft, bool moveConditionRight, bool sprintCondition, bool crouchCondition, bool airControl, bool jumpCondition, double delta, bool IsOnWall, bool IsOnCeiling, bool IsOnFloor) {
         if(!isDead) {
-            FallAndWallSlide((float) delta, wallCheckRight.IsColliding() | wallCheckLeft.IsColliding(), ceilingCheck.IsColliding(), this.IsOnFloor());
-            Jump(Input.IsActionPressed("jump"), this.IsOnFloor());
-            WallJump(Input.IsActionPressed("jump"), wallCheckRight.IsColliding() | (wallCheckLeft).IsColliding());
-            MoveSprintCrouch(Input.IsActionPressed("left"), Input.IsActionPressed("right"), Input.IsActionPressed("sprint"), Input.IsActionPressed("crouch"), airControl, this.IsOnFloor());
+            FallAndWallSlide((float) delta, IsOnWall, IsOnCeiling, IsOnFloor);
+            Jump(jumpCondition, IsOnFloor);
+            WallJump(jumpCondition, IsOnWall);
+            MoveSprintCrouch(moveConditionLeft, moveConditionRight, sprintCondition, crouchCondition, airControl, IsOnFloor);
+
+            Velocity = velocity;
+            MoveAndSlide();
+        }
+    }
+    
+    void FallAndWallSlide(float delta, bool IsOnWall, bool IsOnCeiling, bool IsOnFloor) {
+        if(IsOnWall) {
+            velocity.Y += delta * (wallSlideGravity * 100f);
+        } else if(!IsOnFloor | IsOnCeiling) {
+            velocity.Y += delta * (gravity * 100f);
+        }
+    }
+
+    void Jump(bool jumpCondition, bool IsOnFloor) {
+        if(jumpCondition && IsOnFloor) {
+            velocity.Y = -jumpForce;
+        }
+    }
+
+
+    void WallJump(bool wallClimbJump, bool IsOnWall) {
+        if(!IsOnWall) {
+            this.canWallJump = true;
+        }
+        if(wallClimbJump && IsOnWall && this.canWallJump) {
+            velocity.Y = -wallJumpFroce;
+            this.canWallJump = false;
         }
     }
 
@@ -68,36 +115,18 @@ public partial class Player : CharacterBody2D {
         } else {
             CrouchCollider(false);
         }
-        Velocity = velocity;
-        MoveAndSlide();
-    }
-
-    void Jump(bool jumpCondition, bool IsOnFloor) {
-        if(jumpCondition && IsOnFloor) {
-            velocity.Y = -jumpForce;
-        }
-    }
-
-    void FallAndWallSlide(float delta, bool IsOnWall, bool IsOnCeiling, bool IsOnFloor) {
-        if(IsOnWall) {
-            velocity.Y += delta * (wallSlideGravity * 100f);
-        } else if(!IsOnFloor | IsOnCeiling) {
-            velocity.Y += delta * (gravity * 100f);
-        }
-    }
-
-    void WallJump(bool wallClimbJump, bool IsOnWall) {
-        if(!IsOnWall) {
-            this.canWallJump = true;
-        }
-        if(wallClimbJump && IsOnWall && this.canWallJump) {
-            velocity.Y = -wallJumpFroce;
-            this.canWallJump = false;
-        }
     }
 
     void Flip(bool IsFacingRight) {
-        
+        if(IsFacingRight && this.Scale.X == -1) {
+            wasFlipped = false;
+        } else if(!IsFacingRight && this.Scale.X == 1) {
+            wasFlipped = false;
+        } else if(!IsFacingRight && this.Scale.X == -1) {
+            wasFlipped = true;
+        } else if(IsFacingRight && this.Scale.X == 1) {
+            wasFlipped = true;
+        }
     }
 
     public void RemoveHp(float hp) {
